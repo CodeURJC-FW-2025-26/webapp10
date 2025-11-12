b7bad7cc532a9868284a6fae67ed1d1914712c50
FranGG <fran.garcia.gon06@gmail.com>
2025-11-12 18:31:32 +0100

    Adding express to the form

--- src/router.js
+++ src/router.js
@@ -98,3 +98,69 @@ router.get('/game/:id/image', async (req, res) => {
     res.download(catalog.UPLOADS_FOLDER + '/' + game.imageFilename);
 
 });
+
+router.post('/game/create', upload.single('image'), async (req, res) => {
+
+    let game_create = {
+        videogame_name: req.body.videogame_name,
+
+        videogame_description: req.body.videogame_description,
+
+        videogame_secondary_description: req.body.videogame_secondary_description,
+
+        videogame_developer: req.body.videogame_developer,
+
+        videogame_editor: req.body.videogame_editor,
+
+        videogame_image: req.file?.filename,
+
+        videogame_cost: req.body.videogame_cost,
+
+        videogame_release_date: req.body.videogame_release_date,
+        
+        videogame_platforms: {
+            platform_PlayStation: req.body.platform_PlayStation === 'on',
+            platform_Xbox: req.body.platform_Xbox === 'on',
+            platform_Nintendo: req.body.platform_Nintendo === 'on',
+            platform_PCs: req.body.platform_PCs === 'on',
+            platform_Mobiles: req.body.platform_Mobiles === 'on',
+            platform_VR: req.body.platform_VR === 'on',
+            platform_Arcade: req.body.platform_Arcade === 'on',
+        },
+
+        videogame_modes: {
+            mode_SinglePlayer: req.body.mode_SinglePlayer === 'on',
+            mode_MultiPlayer: req.body.mode_MultiPlayer === 'on',
+            mode_Cooperative: req.body.mode_Cooperative === 'on',
+            mode_Competitive: req.body.mode_Competitive === 'on',
+            mode_Practice: req.body.mode_Practice === 'on',
+            mode_Story: req.body.mode_Story === 'on',
+        },
+
+        age_classification: req.body.age_classification,
+
+        videogame_qualification: req.body.videogame_qualification,
+
+        videogame_genres: {
+            genre_Survival: req.body.genre_Survival === 'on',
+            genre_ActionAdventure: req.body.genre_ActionAdventure === 'on',
+            genre_Strategy: req.body.genre_Strategy === 'on',
+            genre_Sandbox: req.body.genre_Sandbox === 'on',
+            genre_Sports: req.body.genre_Sports === 'on',
+            genre_Simulation: req.body.genre_Simulation === 'on',
+            genre_Puzzle: req.body.genre_Puzzle === 'on',
+            genre_RPG: req.body.genre_RPG === 'on',
+            genre_Horror: req.body.genre_Horror === 'on',
+            genre_BattleRoyale: req.body.genre_BattleRoyale === 'on',
+            genre_Racing: req.body.genre_Racing === 'on',
+            genre_Indie: req.body.genre_Indie === 'on',
+            genre_Shooters: req.body.genre_Shooters === 'on',
+            genre_OpenWorld: req.body.genre_OpenWorld === 'on',
+        }
+    };
+
+    await catalog.addGame(game_create);
+
+    res.render('saved_game', { _id: game_create._id.toString() });
+
+});
